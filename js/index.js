a027d5cd608762d2a6c3c0c6df1402e9fc1e8494
Mori Che Finlayson-Johnecheck <35340393+Irom1@users.noreply.github.com>
2019-05-07 20:42:13 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -16,15 +16,3 @@
   $('nav ul').toggle();
  });
 })(jQuery);
-
-document.onkeyup = function(e) {
-  if (e.which == 77) {
-    alert("M key was pressed");
-  } else if (e.ctrlKey && e.which == 66) {
-location.replace("https://google.com");
-  } else if (e.ctrlKey && e.altKey && e.which == 89) {
-    alert("Ctrl + Alt + Y shortcut combination was pressed");
-  } else if (e.ctrlKey && e.altKey && e.shiftKey && e.which == 85) {
-    alert("Ctrl + Alt + Shift + U shortcut combination was pressed");
-  }
-};
